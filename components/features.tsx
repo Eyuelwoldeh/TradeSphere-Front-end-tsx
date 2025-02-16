f73b5bbed68590faf9b7a6b152f721cfabdaf203
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-02-15 20:30:04 -0600

    removed testimonials

--- components/features.tsx
+++ components/features.tsx
@@ -40,7 +40,7 @@ export default function Features() {
               </span>
             </div>
             <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Thanks for testing this rodak!
+              Big thanks to our founder and CEO Rodak Tehwodros!
             </h2>
           </div>
           <div className="flex justify-center pb-4 md:pb-12" data-aos="fade-up">
