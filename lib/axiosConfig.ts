0b7f4f9504c1f309a12262b97f548598acc63d8b
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-02-15 19:27:35 -0600

    Optimized for Docker containerization

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAuthToken } from "./auth";
 
-const API_BASE_URL = "https://tradesphere-u7ys.onrender.com/api"; // Spring Boot backend (Now deployed on render)
+const API_BASE_URL = "https://trade-sphere-a7edd2d228b9.herokuapp.com/api/"; // Spring Boot backend (Now deployed on render)
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
