377d609491fb4c3e7293f1c81c3270a1bcec30df
eyuelwoldehanna <eyuelwoldehanna@gmail.com>
2025-04-25 11:11:05 -0500

    New Build

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -12,7 +12,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              International Trade Tools
+              Buy My Course Alef!!! Only $20,000 per session.
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
